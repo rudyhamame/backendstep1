f3fdcefc1d9502366684abb4a7d4f8661e063f23
rudyhamame <rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.com>
2021-01-19 09:56:35 -0500

    initial commit

---
+++ models/ninja.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//create Ninja Schema and model
+const NinjaSchema = new Schema({
+  name: { type: String, required: [true, "Name field is required"] },
+  rank: { type: String },
+  available: { type: Boolean, default: false },
+});
+const Ninja = mongoose.model("ninja", NinjaSchema);
+module.exports = Ninja;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const routes = require("./routes/api");
+const mongoose = require("mongoose");
+require("dotenv/config");
+//WothiQggDIPmqBYc
+//connect to mongoDB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  { useUnifiedTopology: true }
+);
+
+const app = express(); // initialie express
+//we use this middleware to access the body of the request
+
+app.use(express.json());
+
+//initialize routes
+app.use("/api", routes);
+
+//error handling middleware
+app.use(function (err, req, res, next) {
+  res.status(422).send({ error: err.message });
+});
+
+app.listen(process.env.PORT || 4000, function () {
+  console.log("now listening on port 4000");
+});
