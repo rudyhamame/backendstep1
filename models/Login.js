2407419ab5bec3cc70760d8fea1b7b21b14ab9e2
rudyhamame <rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.com>
2021-01-22 00:08:10 -0500

    commit

--- models/Login.js
+++ models/Login.js
@@ -5,6 +5,9 @@ const Schema = mongoose.Schema;
 const LoginSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  email: { type: String, required: true },
+  dob: { type: Date, required: true },
+  phone_number: { type: Number, required: false },
 });
 const LoginModel = mongoose.model("Login", LoginSchema);
 module.exports = LoginModel;
