f6584030d29fd14a77020a90744da2a5017ad2ee
rudyhamame <rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.com>
2021-01-27 07:58:39 -0500

    commit

--- routes/TodoAPI.js
+++ routes/TodoAPI.js
@@ -5,7 +5,7 @@ const TodoRouter = express.Router();
 
 //get a list of ninjas the the db
 TodoRouter.get("/Todo", function (req, res, next) {
-  TodoSchema.find({}, { deadline: 0 }).then((items) => res.json(items));
+  TodoSchema.find({ task: "rudy" }).then((items) => res.json(items));
 });
 
 //app a new nonja to the db
