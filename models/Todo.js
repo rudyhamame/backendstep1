1e144e016a5bda0749048d1410c8a9908378c5a2
rudyhamame <rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.com>
2021-01-27 02:22:24 -0500

    commit

--- models/Todo.js
+++ models/Todo.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const TodoSchema = new Schema({
   task: { type: String, required: true },
   deadline: { type: Date, required: true },
-  status: { type: String, required: true },
 });
 const Todo = mongoose.model("Todo", TodoSchema);
 module.exports = Todo;
