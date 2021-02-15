077007cb5235f5b66f3d8aa0d96010c64020c1d5
rudyhamame <rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.com>
2021-02-15 17:16:29 -0500

    commit

--- routes/UserAPI.js
+++ routes/UserAPI.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const UserModel = require("../models/Users");
 const UserRouter = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv/config");
 const checkAuth = require("../check-auth");
