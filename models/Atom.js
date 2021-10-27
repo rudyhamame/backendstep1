0ac359538d2347513923920e462bc67785f620a1
rudyhamame <rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.com>
2021-10-27 12:04:56 +0300

    dfd

--- models/Atom.js
+++ models/Atom.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const AtomSchema = new Schema({
   atomSymbol: { type: String, required: true },
-  electronegativity: { type: String, required: true },
+  electronegativity: { type: Number, required: true },
 });
 const AtomModel = mongoose.model("posts", AtomSchema);
 module.exports = AtomModel;
