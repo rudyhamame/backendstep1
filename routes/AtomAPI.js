2de8f212252bb82a11f04dacfc444c8a321c6d8c
rudyhamame <rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.comgit config --global user.email rudyhamameca@gmail.com>
2021-10-27 12:31:33 +0300

    e

--- routes/AtomAPI.js
+++ routes/AtomAPI.js
@@ -5,9 +5,16 @@ const AtomRouter = express.Router();
 AtomRouter.post("/newAtom/", function (req, res, next) {
   AtomModel.create(req.body)
     .then((result) => {
-      res.status(201).json(result);
+      if (result) res.status(201).json(result);
     })
     .catch(next);
 });
 
+AtomRouter.get("/getAtom/", function (req, res, next) {
+  AtomModel.find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(next);
+});
 module.exports = AtomRouter;
